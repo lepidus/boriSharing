cf97cec6784f64824abfe8c0b9357a16411a905b
AlineLepidus <aline@lepidus.com.br>
2021-12-27 16:48:16 -0400

    Corrects functional tests
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#228
    
    Signed-off-by: AlineLepidus <aline@lepidus.com.br>

--- cypress/tests/functional/Test3_BoriAPIToggleToOff.spec.js
+++ cypress/tests/functional/Test3_BoriAPIToggleToOff.spec.js
@@ -1,25 +1,24 @@
 
-function testExistCheckboxToAPIWorking() {
+function testExistCheckboxToToggleAPIUse() {
     cy.get('#disableAPI').should('exist');
 }
 
 function testExistNotificationToUser(){
-    cy.get('#disableAPI').check();
     cy.get('button[id^=submitFormButton]').contains('Save').click();
     cy.get('.app__notifications > :nth-child(1) > :nth-child(1)').contains('Terms accepted successfully').should('exist');
-    cy.get('.app__notifications > :nth-child(1) > :nth-child(2)').contains('Disabled API').should('exist');
+    cy.get('.app__notifications > :nth-child(1) > :nth-child(2)').contains('Enabled API').should('exist');
     cy.get('.app__notifications > :nth-child(1) > :nth-child(3)').contains('Plugin working').should('exist');
 }
 
-function testEnableAPI(){
+function testDisabledAPI(){
     cy.get('a[id^=component-grid-settings-plugins-settingsplugingrid-category-generic-row-borisharingplugin-settings-button]').click();
-    cy.get('#disableAPI').uncheck();
+    cy.get('#disableAPI').check();
     cy.get('button[id^=submitFormButton]').contains('Save').click();
-    cy.get('.app__notifications > :nth-child(1) > :nth-child(4)').contains('Enabled API').should('exist');
+    cy.get('.app__notifications > :nth-child(1) > :nth-child(4)').contains('Disabled API').should('exist');
 }
 
 describe('BoriSharing Plugin auth key test', function() {
-    it('Adds turn off API tests', function() {
+    it('Adds API toggle tests: start enabled', function() {
         cy.visit(Cypress.env('baseUrl') + 'index.php/h/submissions');
         cy.get('input[id=username]').click();
         cy.get('input[id=username]').type(Cypress.env('OJSAdminUsername'), { delay: 0 });
@@ -36,11 +35,11 @@ describe('BoriSharing Plugin auth key test', function() {
         cy.get('input[id^=userAuthKey]').click();
         cy.get('input[id^=userAuthKey]').type( Cypress.env('UserAuthKey'), { delay: 2 });
         
-        testExistCheckboxToAPIWorking();
+        testExistCheckboxToToggleAPIUse();
 
         testExistNotificationToUser();
 
-        testEnableAPI()
+        testDisabledAPI()
 
     });
 
