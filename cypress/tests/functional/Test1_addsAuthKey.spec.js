1e9ebbe590fa9c7554d2795cd64c4d417fd184c1
Jhon <jhon@lepidus.com.br>
2022-02-01 14:13:58 -0400

    Fixes first integration test
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#248
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Joao Vitor <joaovitor@lepidus.com.br>

--- cypress/tests/functional/Test1_addsAuthKey.spec.js
+++ cypress/tests/functional/Test1_addsAuthKey.spec.js
@@ -13,20 +13,19 @@ function testAcceptPrivacyTermsWithAuth() {
     cy.get('input[id^=userAuthKey]').click();
     cy.get('input[id^=userAuthKey]').type( Cypress.env('UserAuthKey'), { delay: 1 });
     cy.get('button[id^=submitFormButton]').contains('Save').click();
-    cy.get('.app__notifications > :nth-child(1) > :nth-child(2)').contains('Plugin working').should('exist');
+    cy.get('.pkpNotification').contains('Plugin working').should('exist');
 }
 
 
 describe('BoriSharing Plugin auth key test', function() {
     it('Adds auth key tests', function() {
-        cy.visit(Cypress.env('baseUrl') + 'index.php/h/submissions');
+        cy.visit(Cypress.env('baseUrl') + 'index.php/anphlac/management/settings/website');
         cy.get('input[id=username]').click();
         cy.get('input[id=username]').type(Cypress.env('OJSAdminUsername'), { delay: 0 });
         cy.get('input[id=password]').click();
         cy.get('input[id=password]').type(Cypress.env('OJSAdminPassword'), { delay: 0 });
         cy.get('button[class=submit]').click();
 
-        cy.get(':nth-child(3) > ul > :nth-child(2) > .app__navItem').click();
         cy.get('#plugins-button').click();
         cy.get('#component-grid-settings-plugins-settingsplugingrid-category-generic-row-borisharingplugin > .first_column > .show_extras').click();
         cy.get('a[id^=component-grid-settings-plugins-settingsplugingrid-category-generic-row-borisharingplugin-settings-button]').click();
